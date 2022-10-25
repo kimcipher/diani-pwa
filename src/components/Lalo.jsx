2684193a3493165e76587061def91b990d8eec06
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-10-25 12:51:05 +0300

    update lalo modal

--- src/components/Lalo.jsx
+++ src/components/Lalo.jsx
@@ -1,30 +1,30 @@
 import React, { useState, useEffect } from "react";
 import {
-  foodwhite,
-  drinkwhite,
-  grocerieswhite,
-  credit,
   line,
-  live,
   oneeight,
   fourth,
-  twentyeight,
   people,
   location,
   bookings,
   shopping,
   livecon,
   lalologo,
-  kulosa
+  kulosa,
+  tickItem,
+  starhundred,
+  stareighty,
 } from "./icons";
 import "./css/Lalo.css";
 import { Carousel } from "react-responsive-carousel";
 import Collapsible from "react-collapsible";
+import PWA from "./assets/PWA.png"
+import UDL from "./assets/udl.png"
+import webthree from "./assets/Web3.png"
 
 const Lalo = (props) => {
   const d = new Date();
   let hour = d.getHours();
-const bookings_idx = hour + (hour*5)
+  const bookings_idx = hour + hour * 5;
   console.log(props);
   // function clicked(){
   //     setItIsOpen(false)
@@ -34,44 +34,75 @@ const bookings_idx = hour + (hour*5)
       <div className="child-closer" onClick={() => props.setItIsOpen(false)}>
         <div className="closest">{kulosa}</div>
       </div>
-      <Carousel>
+      <Carousel
+      // dynamicHeight={true}
+      showIndicators={false}
+      className="carry"
+      showStatus={false}
+      >
         <div>
-          <div className="child" style={{ marginTop: "20px" }}>
-            <p style={{textTransform:"uppercase", fontSize:"0.7em"}}>
-              {lalologo}<br/>
-              LALO COMMUNITIES is AN ALWAYS ON &nsbp; |
-              <br /> 24/7 INSTANT WEB3 BOOKING DAPP
-              <br /> powering small communities & THEIR <br /> VISITORS 🌎.
-              <br />
-              <br /> Deliveries. Bookings . Anything Anytime
-            </p>
-          </div>
-          <div className="child">
-            <div className="child-container">
-              <div className="child-container-child">{foodwhite} Food</div>
-              <div className="child-container-child">{drinkwhite} Drinks</div>
-              <div className="child-container-child">
-                {grocerieswhite} Groceries
-              </div>
-              {/* <div className="child-container-child">{credit} Bookings</div> */}
-              <div className="child-container-child">{shopping} Shopping</div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-around",
+              width: "150px",
+              maxHeight:"50px",
+              lineHeight:"2px",
+              marginBottom:"20px"
+
+
+            }}
+          >
+            {lalologo} &nbsp;{" "}
+            <div style={{ textAlign: "left"}}>
+              <p style={{ textAlign: "left", color: "#fff", lineHeight:"10px" }}>Lalo</p>
+              <p style={{ textAlign: "left", color: "#fff", lineHeight:"10px" }}>Communities</p>
             </div>
           </div>
-          <div className="child">
-            <h3
-              style={{
-                color: "#fff",
-                textAlign: "center",
-                marginLeft: "-50px",
-                fontWeight: "bold",
-              }}
-            >
-              Try Us!
-            </h3>
+          <div style={{marginBottom:"20px"}}>
+            <h4>
+              Web3 & Blockchain based platform enabling bookings + deliveries.
+            </h4>
           </div>
-          {/* <div className="child spec">
-                    More {more}
-                </div> */}
+          <div style={{display:"block"}}>
+          <div className="descriptor">
+            {tickItem}
+            <h6>PWA, install it to your phone
+            PWA, install it to your phone
+            </h6>
+          </div>
+          <div className="descriptor">
+            {tickItem}
+            <h6>
+              Order anything + anytime at the comfort of your existing apps
+              (WhatsApp)
+            </h6>
+            </div>
+            <div className="descriptor">
+              {tickItem}
+              <h6>
+                An array of payment options including MPESA, Credit Cards,
+                PayPal and Crypto.
+              </h6>
+            </div>
+            <div className="descriptor">
+              {tickItem}
+              <h6>
+                Connect various local and international tourists to services
+                providers.
+              </h6>
+            </div>
+            <div style={{ display: "flex", flexDirection: "row", justifyContent:"space-around" }}>
+              {stareighty}
+              {starhundred}
+            </div>
+            <div style={{ display: "flex", flexDirection: "row", justifyContent:"space-around", width:"auto", margin:"auto", marginTop:"30px" }}>
+              <img src={webthree} style={{width:"50px"}}/>
+              <img src={UDL} style={{width:"150px"}}/>
+              <img src={PWA} style={{width:"50px"}}/>
+            </div>
+          </div>
         </div>
         <div>
           {/* <div className="child" onClick={() => setItIsOpen(false)}>
@@ -91,22 +122,42 @@ const bookings_idx = hour + (hour*5)
 
             <div className="blink"></div>
           </div>
-          <div className="child" style={{ marginTop: "20px", display:"flex",justifyContent:'space-around' }}>
+          <div
+            className="child"
+            style={{
+              marginTop: "20px",
+              display: "flex",
+              justifyContent: "space-around",
+            }}
+          >
             <p>
-              {oneeight}<br/>
+              {oneeight}
+              <br />
               {people}
             </p>
-            <p style={{marginTop:"25px"}}>
-              &nbsp;&nbsp;&nbsp;&nbsp;{location}<br/>
+            <p style={{ marginTop: "25px" }}>
+              &nbsp;&nbsp;&nbsp;&nbsp;{location}
+              <br />
               {fourth}
             </p>
-            <p style={{marginTop:"25px"}}>
-            &nbsp;&nbsp;&nbsp;&nbsp;{bookings}<br/>
-            
-              <h1 style={{fontSize:"3em", color:"#fff", marginTop:"45px", fontWeight:"bold"}}>{bookings_idx}</h1>
-              <p style={{color:"#fff", paddingTop:"20px"}}>Bookings<br/> made</p>
+            <p style={{ marginTop: "25px" }}>
+              &nbsp;&nbsp;&nbsp;&nbsp;{bookings}
+              <br />
+              <h1
+                style={{
+                  fontSize: "3em",
+                  color: "#fff",
+                  marginTop: "45px",
+                  fontWeight: "bold",
+                }}
+              >
+                {bookings_idx}
+              </h1>
+              <p style={{ color: "#fff", paddingTop: "20px" }}>
+                Bookings
+                <br /> made
+              </p>
             </p>
-            
           </div>
           <div className="child">
             <h3
@@ -131,7 +182,7 @@ const bookings_idx = hour + (hour*5)
           <div className="child">
             <div className="child-container">
               <div className="child-container-child-spec">
-                <h3 style={{fontSize:"0.9em"}}>EAST AFRICA</h3>
+                <h3 style={{ fontSize: "0.9em" }}>EAST AFRICA</h3>
                 {/* <p> &nbsp; &nbsp; {livecon}&nbsp; &nbsp; KENYA</p> */}
                 <details className="collapsible">
                   <summary>KE 🇰🇪 </summary>
@@ -141,16 +192,16 @@ const bookings_idx = hour + (hour*5)
                   <p>&nbsp; &nbsp; &nbsp; &nbsp;Malindi</p>
                 </details>
                 <details className="collapsible">
-                    <summary>UG 🇺🇬 </summary>
-                    <p>&nbsp; &nbsp; &nbsp; &nbsp;Jinja</p>
+                  <summary>UG 🇺🇬 </summary>
+                  <p>&nbsp; &nbsp; &nbsp; &nbsp;Jinja</p>
                 </details>
                 <details className="collapsible">
                   <summary>TZ 🇹🇿 </summary>
-                    <p>&nbsp; &nbsp; &nbsp; &nbsp;Zanzibar</p>
+                  <p>&nbsp; &nbsp; &nbsp; &nbsp;Zanzibar</p>
                 </details>
               </div>
               <div className="child-container-child-spec">
-                <h3 style={{fontSize:"0.9em"}}>CARRIBEAN</h3>
+                <h3 style={{ fontSize: "0.9em" }}>CARRIBEAN</h3>
                 <details className="collapsible">
                   <summary>BAMA 🇧🇸</summary>
                   <p>&nbsp; &nbsp; &nbsp; &nbsp;Goombay</p>
@@ -160,24 +211,44 @@ const bookings_idx = hour + (hour*5)
                 </details>
               </div>
               <div className="child-container-child-spec">
-                <h3 style={{fontSize:"0.9em"}}>AMERICAS</h3>
-                <p className="small" style={{fontSize:"0.5em", textAlign:"center", marginBottom:"0"}}> &nbsp; &nbsp; (coming soon)</p>
-                  <details className="collapsible">
-                    <summary>CR 🇨🇷</summary>
-                  </details>
-                  <details className="collapsible">
-                    <summary>ELSL 🇸🇻</summary>
-                  </details>
+                <h3 style={{ fontSize: "0.9em" }}>AMERICAS</h3>
+                <p
+                  className="small"
+                  style={{
+                    fontSize: "0.5em",
+                    textAlign: "center",
+                    marginBottom: "0",
+                  }}
+                >
+                  {" "}
+                  &nbsp; &nbsp; (coming soon)
+                </p>
+                <details className="collapsible">
+                  <summary>CR 🇨🇷</summary>
+                </details>
+                <details className="collapsible">
+                  <summary>ELSL 🇸🇻</summary>
+                </details>
               </div>
               <div className="child-container-child-spec">
-                <h3 style={{fontSize:"0.9em"}}>ASIA</h3>
-                <p className="small" style={{fontSize:"0.5em", textAlign:"center", marginBottom:"0" }}> &nbsp; &nbsp; (coming soon)</p>
+                <h3 style={{ fontSize: "0.9em" }}>ASIA</h3>
+                <p
+                  className="small"
+                  style={{
+                    fontSize: "0.5em",
+                    textAlign: "center",
+                    marginBottom: "0",
+                  }}
+                >
+                  {" "}
+                  &nbsp; &nbsp; (coming soon)
+                </p>
                 <details className="collapsible">
-                    <summary>IDN 🇮🇩</summary>
-                  </details>
-                  <details className="collapsible">
-                    <summary>TH 🇹🇭</summary>
-                  </details>
+                  <summary>IDN 🇮🇩</summary>
+                </details>
+                <details className="collapsible">
+                  <summary>TH 🇹🇭</summary>
+                </details>
               </div>
             </div>
           </div>
