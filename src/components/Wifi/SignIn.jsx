4552125fe20284cb9b11d17dca0afe7382328944
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-11-09 13:16:54 +0300

    rmreact-bootstrap

--- src/components/Wifi/SignIn.jsx
+++ src/components/Wifi/SignIn.jsx
@@ -1,7 +1,5 @@
 import React, {useState} from 'react'
 import {Link} from "react-router-dom"
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import {useNavigate} from  "react-router-dom"
 import axios from 'axios';
 
