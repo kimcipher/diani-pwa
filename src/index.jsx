fc123209018ae5df251467a5dd9cd83367cb2897
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-11-14 14:06:02 +0300

    update build

--- src/index.jsx
+++ src/index.jsx
@@ -5,15 +5,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./registerServiceWorker";
 import PWAPrompt from "react-ios-pwa-prompt";
-import "@rainbow-me/rainbowkit/styles.css";
-import {
-  // getDefaultWallets,
-  // RainbowKitProvider,
-  // darkTheme,
-} from "@rainbow-me/rainbowkit";
-// import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-// import { alchemyProvider } from "wagmi/providers/alchemy";
-// import { publicProvider } from "wagmi/providers/public";
 import reportWebVitals from "./reportWebVitals";
 import ReactPwa from "react-pwa-app";
 import { ClearBrowserCacheBoundary } from "react-clear-browser-cache";
